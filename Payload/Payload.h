ad05a2d324e1fa81fda07fdf65c410af432e5059
Ian Halpern <ian.h.halpern@gmail.com>
2020-09-22 17:01:32 -0400

    Initial Commit

---
+++ Payload/Payload.h
@@ -0,0 +1,18 @@
+//
+//  Payload.h
+//  Payload
+//
+//  Created by Ian Halpern on 9/22/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Payload.
+FOUNDATION_EXPORT double PayloadVersionNumber;
+
+//! Project version string for Payload.
+FOUNDATION_EXPORT const unsigned char PayloadVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Payload/PublicHeader.h>
+
+
